6e90c3e97d7e23137de81b58b00326af7b731e74
Yefim Vedernikoff <yefim323@gmail.com>
2019-01-26 20:23:06 -0500

    Fix gulp first-try

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,67 @@
-var gulp = require('gulp');
+var {src, dest, parallel} = require('gulp');
+var gulpSass = require('gulp-sass');
 var connect = require('gulp-connect');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
 var autoprefixer = require('gulp-autoprefixer');
 var cssmin = require('gulp-cssmin');
 var htmlmin = require('gulp-htmlmin');
 
-gulp.task('serve', function() {
-  connect.server({
+gulpSass.compiler = require('node-sass');
+
+exports.serve = function serve() {
+  return connect.server({
     livereload: true,
     port: 7777
   });
-});
+};
 
-gulp.task('html', function() {
-  gulp.src('./*.html')
+exports.sass = function sass() {
+  return src('./styles/*.scss')
+    .pipe(gulpSass.sync().on('error', gulpSass.logError))
+    .pipe(dest('./dist'))
     .pipe(connect.reload());
-});
+};
 
-gulp.task('sass', function() {
-  gulp.src('./styles/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'))
+exports.html = function html() {
+  return src('./*.html')
     .pipe(connect.reload());
-});
-
-gulp.task('watch', function() {
-  gulp.watch('./styles/*.scss', ['sass']);
-  gulp.watch('./*.html', ['html']);
-});
+};
 
 // Development
-gulp.task('default', ['serve', 'sass', 'html', 'watch']);
+exports.default = parallel([exports.serve, exports.sass, exports.html]);
 
 // Production builds
-gulp.task('sassmin', function() {
-  gulp.src('./styles/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
+exports.sassmin = function sassmin() {
+  return src('./styles/*.scss')
+    .pipe(gulpSass.sync().on('error', gulpSass.logError))
     .pipe(autoprefixer())
     .pipe(cssmin())
-    .pipe(gulp.dest('./out/dist'));
-});
+    .pipe(dest('./out/dist'));
+};
 
-gulp.task('htmlmin', function() {
-  gulp.src('./*.html')
+exports.htmlmin = function htmlmin() {
+  return src('./*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./out'));
-});
+    .pipe(dest('./out'));
+};
 
-gulp.task('images', function() {
-  gulp.src('./images/*.png')
-    .pipe(gulp.dest('./out/images'));
-});
+exports.images = function images() {
+  return src('./images/*.png')
+    .pipe(dest('./out/images'));
+};
 
-gulp.task('vendor', function() {
-  gulp.src('./vendor/*')
-    .pipe(gulp.dest('./out/vendor'));
-});
+exports.vendor = function vendor() {
+  return src('./vendor/*')
+    .pipe(dest('./out/vendor'));
+};
 
-gulp.task('favicons', function() {
-  gulp.src('./favicons/*')
-    .pipe(gulp.dest('./out'));
-});
+exports.favicons = function favicons() {
+  return src('./favicons/*')
+    .pipe(dest('./out'));
+};
 
-gulp.task('files', function() {
-  gulp.src('./files/*')
-    .pipe(gulp.dest('./out/files'));
-});
+exports.files = function files() {
+  return src('./files/*')
+    .pipe(dest('./out/files'));
+};
 
-gulp.task('build', ['sassmin', 'htmlmin', 'images', 'vendor', 'favicons', 'files']);
+exports.build = parallel([exports.sassmin, exports.htmlmin, exports.images, exports.vendor, exports.favicons, exports.files]);
